50ce8e4df3a0a382177f7b36549ba067d03eb57d
badamPss <ignatvaschilo@yandex.ru>
2026-03-07 16:51:13 +0300

    [feat] add struct of handlers for advertiser

---
+++ internal/handlers/entry.go
@@ -0,0 +1,13 @@
+package handlers
+
+import "github.com/gorilla/mux"
+
+type HTTPController interface {
+	RegisterRoutes(router *mux.Router)
+}
+
+func Register(r *mux.Router, controllers ...HTTPController) {
+	for _, controller := range controllers {
+		controller.RegisterRoutes(r)
+	}
+}

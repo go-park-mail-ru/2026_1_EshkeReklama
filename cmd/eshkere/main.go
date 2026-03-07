46badccb212166f84cd93818c2312603ff725224
badamPss <ignatvaschilo@yandex.ru>
2026-03-07 03:10:15 +0300

    [feat] add config + server

--- cmd/eshkere/main.go
+++ cmd/eshkere/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"eshkere/internal/app"
+	"flag"
+	"log"
+)
+
+var configPath = flag.String("config", "./config/config.yaml", "path to config file")
+
+func main() {
+	flag.Parse()
+
+	application := app.New(*configPath)
+	if err := application.Run(); err != nil {
+		log.Fatal(err)
+	}
+}

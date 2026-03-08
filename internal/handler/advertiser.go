8c5df53df9caf0589b31394d5c3119a6adb4c7a1
badamPss <ignatvaschilo@yandex.ru>
2026-03-08 23:24:50 +0300

    [feat] add sessions

--- internal/handler/advertiser.go
+++ internal/handler/advertiser.go
@@ -37,8 +37,21 @@ func (a *API) Register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	advertiser := ToRegisterAdvertiserModel(req)
-	httpx.JSON(w, http.StatusOK, advertiser)
+	// временно считаем, что пользователь создан
+	advertiserID := 1
+
+	if err := a.sessionManager.Create(w, advertiserID); err != nil {
+		httpx.InternalError(w)
+		return
+	}
+
+	resp := dto.RegisterResponse{
+		ID:    advertiserID,
+		Email: req.Email,
+		Phone: req.Phone,
+	}
+
+	httpx.JSON(w, http.StatusOK, resp)
 }
 
 func (a *API) Login(w http.ResponseWriter, r *http.Request) {
@@ -54,10 +67,27 @@ func (a *API) Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	advertiser := ToLoginAdvertiserModel(req)
-	httpx.JSON(w, http.StatusOK, advertiser)
+	// Временно считаем, что пользователь найден и пароль верный
+	advertiserID := 1
+
+	if err := a.sessionManager.Create(w, advertiserID); err != nil {
+		httpx.InternalError(w)
+		return
+	}
+
+	resp := dto.LoginResponse{
+		ID:    advertiserID,
+		Email: "test@example.com",
+		Phone: "+79991234567",
+	}
+
+	httpx.JSON(w, http.StatusOK, resp)
 }
 
 func (a *API) Logout(w http.ResponseWriter, r *http.Request) {
-	return
+	a.sessionManager.Destroy(w, r)
+
+	httpx.JSON(w, http.StatusOK, map[string]string{
+		"message": "logout ok",
+	})
 }

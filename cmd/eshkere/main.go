c258752d1e8ecc3673d91df2d8fbf08359151191
badamPss <ignatvaschilo@yandex.ru>
2026-03-09 13:56:21 +0300

    add env settings

--- cmd/eshkere/main.go
+++ cmd/eshkere/main.go
@@ -4,12 +4,19 @@ import (
 	"eshkere/internal/app"
 	"flag"
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 var configPath = flag.String("config", "./config/config.yaml", "path to config file")
 
 func main() {
 	flag.Parse()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file not found")
+	}
 
 	application := app.New(*configPath)
 	if err := application.Run(); err != nil {

15fca0eafa41f2c6c5dd38c95771f78d301a4116
Vovchik <6ladimir@gmail.com>
2026-04-11 20:07:45 +0300

    fix migr

--- db/migrations/000002_daily_budget_bigint.down.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-ALTER TABLE eshkere.ad_campaign
-    ALTER COLUMN daily_budget TYPE DECIMAL(15, 2) USING (daily_budget::DECIMAL / 100),
-    ALTER COLUMN daily_budget SET DEFAULT 0;
-
-COMMIT;

13ee801a0758d32797f2cd0561e4731c7023d180
badamPss <ignatvaschilo@yandex.ru>
2026-05-04 02:44:46 +0300

    some fixes'

---
+++ db/migrations/000010_partner_site_owner_to_advertiser.down.sql
@@ -0,0 +1,10 @@
+BEGIN;
+
+ALTER TABLE eshkere.partner_site
+    DROP CONSTRAINT IF EXISTS partner_site_partner_id_fkey;
+
+ALTER TABLE eshkere.partner_site
+    ADD CONSTRAINT partner_site_partner_id_fkey
+        FOREIGN KEY (partner_id) REFERENCES eshkere.partner(id) ON DELETE CASCADE;
+
+COMMIT;
